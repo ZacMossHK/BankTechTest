591fca30a22b4128a46bfba5a2203649eec16741
Zac <zac@zacmoss.co.uk>
2022-09-19 14:33:09 +0100

    writing tests

---
+++ spec/bankAccountModel.spec.js
@@ -0,0 +1,38 @@
+const BankAccountModel = require("../lib/bankAccountModel");
+
+let bankAccountModel;
+
+describe("bankAccountModel", () => {
+  beforeEach(() => {
+    bankAccountModel = new BankAccountModel();
+  });
+
+  it("saves an object to the model", () => {
+    const transactionObject = {
+      amount: 10.0,
+      transactionDate: new Date(2022, 4, 4),
+      transactionType: "deposit",
+    };
+    bankAccountModel.saveToModel(transactionObject);
+    expect(bankAccountModel.loadFromModel()).toEqual([transactionObject]);
+  });
+
+  it("saves two objects to the model", () => {
+    const transactionObject1 = {
+      amount: 10.0,
+      transactionDate: new Date(2022, 4, 4),
+      transactionType: "deposit",
+    };
+    const transactionObject2 = {
+      amount: 5.0,
+      transactionDate: new Date(2022, 4, 5),
+      transactionType: "withdraw",
+    };
+    bankAccountModel.saveToModel(transactionObject1);
+    bankAccountModel.saveToModel(transactionObject2);
+    expect(bankAccountModel.loadFromModel).toEqual([
+      transactionModel1,
+      transactionModel2,
+    ]);
+  });
+});
