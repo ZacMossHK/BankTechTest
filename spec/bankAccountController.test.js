fcc9760ee9b80cd7fb4c2c31b34c34ed7fabfc4e
Zac <zac@zacmoss.co.uk>
2022-09-20 16:03:44 +0100

    refactored bankaccountcontroller to remove handletransaction methods and relevant tests

--- spec/bankAccountController.test.js
+++ spec/bankAccountController.test.js
@@ -1,26 +1,33 @@
 const BankAccountController = require("../lib/bankAccountController");
 
-let bankAccountController, mockBankAccountModel, mockCreateStatement;
+let bankAccountController,
+  mockBankAccountModel,
+  mockCreateStatement,
+  mockHandleTransaction;
 
 describe("BankAccountController class", () => {
   beforeEach(() => {
     mockBankAccountModel = { saveToModel: jest.fn(), loadFromModel: jest.fn() };
     mockCreateStatement = { getStatement: jest.fn() };
+    mockHandleTransaction = { makeTransaction: jest.fn() };
     bankAccountController = new BankAccountController(
       mockBankAccountModel,
-      mockCreateStatement
+      mockCreateStatement,
+      mockHandleTransaction
     );
   });
 
   it("makes a transaction", () => {
-    const transactionObject = {
-      amount: 10.0,
-      date: new Date(2022, 4, 4),
-      type: "credit",
-    };
-    bankAccountController.makeTransaction("credit", 10.0, new Date(2022, 4, 4));
-    expect(mockBankAccountModel.saveToModel).toHaveBeenCalledWith(
-      transactionObject
+    bankAccountController.handleNewTransaction(
+      "credit",
+      10.0,
+      new Date(2022, 4, 4)
+    );
+    expect(mockHandleTransaction.makeTransaction).toHaveBeenCalledWith(
+      "credit",
+      10.0,
+      new Date(2022, 4, 4),
+      mockBankAccountModel
     );
   });
 
@@ -41,44 +48,4 @@ describe("BankAccountController class", () => {
       transactionObject,
     ]);
   });
-
-  // it("throws an error if transaction type is not credit or debit", () => {
-  //   expect(() => {
-  //     bankAccountController.makeTransaction(
-  //       "balloon",
-  //       100.0,
-  //       new Date(2022, 4, 4)
-  //     );
-  //   }).toThrow(
-  //     new Error('Transaction type must be a string - "credit" or "debit"')
-  //   );
-  // });
-
-  // it("throws an error if transaction amount is not an integer or float", () => {
-  //   expect(() => {
-  //     bankAccountController.makeTransaction(
-  //       "credit",
-  //       "50",
-  //       new Date(2022, 4, 4)
-  //     );
-  //   }).toThrow(new Error("Transaction amount must be an integer or float"));
-  // });
-
-  // it("throws an error if transaction date is not a Date instance", () => {
-  //   expect(() => {
-  //     bankAccountController.makeTransaction("credit", 50, "4/4/2022");
-  //   }).toThrow(new Error("Transaction date must be a date instance"));
-  // });
-
-  // it("throws an error if any transaction arguments are missing", () => {
-  //   expect(() => {
-  //     bankAccountController.makeTransaction("credit", 50);
-  //   }).toThrow(new Error("Missing transaction arguments"));
-  //   expect(() => {
-  //     bankAccountController.makeTransaction("credit");
-  //   }).toThrow(new Error("Missing transaction arguments"));
-  //   expect(() => {
-  //     bankAccountController.makeTransaction();
-  //   }).toThrow(new Error("Missing transaction arguments"));
-  // });
 });
