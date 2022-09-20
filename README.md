d52bc59d23920209e7db3f03a9d38e8df3457c2a
Zac <zac@zacmoss.co.uk>
2022-09-20 13:14:24 +0100

    passed test

--- README.md
+++ README.md
@@ -24,10 +24,10 @@ $ node
 > controller.makeTransaction("debit", 500, new Date(2023, 0, 14));
 > controller.getStatement();
 =>
-date || credit || debit || balance
-14/01/2023 || || 500.00 || 2500.00
-13/01/2023 || 2000.00 || || 3000.00
-10/01/2023 || 1000.00 || || 1000.00
+"date || credit || debit || balance\n
+14/01/2023 || || 500.00 || 2500.00\n
+13/01/2023 || 2000.00 || || 3000.00\n
+10/01/2023 || 1000.00 || || 1000.00"
 ```
 
 The program is structured into two classses:
@@ -40,5 +40,6 @@ To make a transaction call BankAccountController.makeTransaction(type, amount, d
 Type must be a string, "credit" or "debit".
 Amount can be an integer or float.
 Date must be a date instance.
+Giving the wrong or missing arguments will invalidate the transaction and it will not be saved.
 
-To print the statement of transactions up to this point run BankAccountController.getStatement().
+To return the statement of transactions up to this point run BankAccountController.getStatement().
--- spec/integration.spec.js
+++ spec/integration.spec.js
@@ -9,6 +9,12 @@ describe("Integration", () => {
     bankAccountController = new BankAccountController(bankAccountModel);
   });
 
+  it("prints the statement with no transactions", () => {
+    expect(bankAccountController.getStatement()).toBe(
+      "date || credit || debit || balance"
+    );
+  });
+
   it("prints the statement with one transaction", () => {
     bankAccountController.makeTransaction("credit", 10, new Date(2022, 4, 4));
     const result =
@@ -26,6 +32,23 @@ describe("Integration", () => {
       "credit",
       2000,
       new Date(2023, 0, 13)
+    );
+    bankAccountController.makeTransaction("debit", 500, new Date(2023, 0, 14));
+    const result =
+      "date || credit || debit || balance\n14/01/2023 || || 500.00 || 2500.00\n13/01/2023 || 2000.00 || || 3000.00\n10/01/2023 || 1000.00 || || 1000.00";
+    expect(bankAccountController.getStatement()).toBe(result);
+  });
+
+  it("prints the statement in the correct chronological order when transactions are not submitted in order", () => {
+    bankAccountController.makeTransaction(
+      "credit",
+      2000,
+      new Date(2023, 0, 13)
+    );
+    bankAccountController.makeTransaction(
+      "credit",
+      1000,
+      new Date(2023, 0, 10)
     );
     bankAccountController.makeTransaction("debit", 500, new Date(2023, 0, 14));
     const result =
